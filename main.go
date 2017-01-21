6a50df08c2b04d3cc8d73d601db92fb51f741a36
Milorad Bozic <miloradbozic29@gmail.com>
2017-01-21 02:23:38 +0100

    Simple websocket server

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func main() {
-    fmt.Println("Hello from go")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":4000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	socket, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(string(msg))
+		responseMessage := "Recieved message is: " + string(msg)
+
+		if err = socket.WriteMessage(msgType, []byte(responseMessage)); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
